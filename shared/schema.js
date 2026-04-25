72047006c10be23a8add4e8ad6cea45bf8366d58
yugankdas <yugankdas2006@gmail.com>
2026-04-26 00:31:29 +0530

    resolving conflicts

--- shared/schema.js
+++ shared/schema.js
@@ -28,17 +28,11 @@ function createTheme(value) {
         value
     };
 }
-
-function createComponent(name, props = []) {
-    return { type: "component", name, props };
-}
-
 module.exports = {
     createText,
     createButton,
     createColumn,
     createRow,
     createImage,
-    createTheme,
-    createComponent
+    createTheme
 };
